7b2c52b326279c152f16adee74a8269fbf2643eb
mh <mh@immerda.ch>
2008-10-02 23:04:35 +0000

    correct path for openbsd

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -3,14 +3,24 @@
 define sendmail::mailalias(
     $ensure = 'present',
     $recipient,
-    $target = '/etc/aliases'
+    $target = 'absent'
 ){
     include sendmail::newaliases
+
+    case $target {
+        'absent': { 
+            $real_target = $operatingsystem ? {
+                openbsd => '/etc/mail/aliases',
+                default => '/etc/aliases',
+            }
+        }
+        default: { $real_target = $target }
+    }
 
     mailalias{"$name":
         ensure => $ensure,
         recipient => $recipient,
-        target => $target,
+        target => $real_target,
         notify => Exec['refresh_aliases'],
     }
 }
