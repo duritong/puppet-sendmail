46f8b20f764d0333b088476cd3f17bfc3aa26f68
mh <mh@immerda.ch>
2008-10-02 22:14:21 +0000

    include magic

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -1,7 +1,16 @@
 # manifests/disable.pp
 # disable sendmail
 
-class sendmail::disable inherits sendmail::base {
+class sendmail::disable inherits sendmail {
+    case $kernel {
+        linux: { include sendmail::disable::base }
+    }
+    if $use_munin {
+        include sendmail::munin::disable
+    }
+}
+
+class sendmail::disable::base inherits sendmail::base {
     Package[sendmail]{
         ensure => absent,
     }
@@ -9,9 +18,5 @@ class sendmail::disable inherits sendmail::base {
     Service[sendmail]{
         enable => false,
         ensure => stopped,
-    }
-
-    if $use_munin {
-        include sendmail::munin::disable
     }
 }
