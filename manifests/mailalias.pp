12847c50d59a3db4f57f729e96d4ffb3f5bdde91
mh <mh@immerda.ch>
2018-02-18 14:05:49 +0100

    modernize and introduce types to validate params

--- manifests/mailalias.pp
+++ manifests/mailalias.pp
@@ -1,24 +1,31 @@
 define sendmail::mailalias(
-    $ensure = 'present',
-    $recipient,
-    $target = 'absent'
+  Enum['present','absent'] $ensure    = 'present',
+  Optional[String]         $recipient = undef,
+  String                   $target    = 'absent',
 ){
-    include sendmail::newaliases
+  if $ensure == 'present' and !$recipient {
+    fail('Must pass recipient if ensure is present')
+  }
+  include sendmail::newaliases
 
-    case $target {
-        'absent': {
-            $real_target = $::operatingsystem ? {
-                openbsd => '/etc/mail/aliases',
-                default => '/etc/aliases',
-            }
-        }
-        default: { $real_target = $target }
+  case $target {
+    'absent': {
+      $real_target = $::operatingsystem ? {
+        openbsd => '/etc/mail/aliases',
+        default => '/etc/aliases',
+      }
     }
+    default: { $real_target = $target }
+  }
 
-    mailalias{$name:
-        ensure => $ensure,
-        recipient => $recipient,
-        target => $real_target,
-        notify => Exec['refresh_aliases'],
+  mailalias{$name:
+    ensure => $ensure,
+    target => $real_target,
+    notify => Exec['refresh_aliases'],
+  }
+  if $ensure == 'present' {
+    Mailalias[$name]{
+      recipient => $recipient,
     }
+  }
 }
